52cc575153ef6d6be5610c83e38fd01e19ad4222
andrasreka <reka@mollia.hu>
2022-12-11 23:43:52 +0200

    increase sigmas

--- bilateral.py
+++ bilateral.py
@@ -9,7 +9,7 @@ def distance(x, y, i, j): return np.sqrt((x-i)**2 + (y-j)**2)
 
 def gaussian(x, sigma): return (1.0 / (2 * np.pi * (sigma ** 2))) * np.exp(- (x ** 2) / (2 * sigma ** 2))
 
-def apply_bilateral_filter(f, g, x, y, diameter, sigma_i, sigma_s):
+def apply_bilateral_filter(f, g, x, y, diameter, sigma_spatial, sigma_range):
     
     #f usid for weighting
     #g is filtered
@@ -31,8 +31,8 @@ def apply_bilateral_filter(f, g, x, y, diameter, sigma_i, sigma_s):
             mean_i = int(f[int(window_x)][int(window_y)]) - int(f[x][y])
             mean_s = distance(window_x, window_y, x, y)
 
-            gi = gaussian(mean_i, sigma_i)
-            gs = gaussian(mean_s, sigma_s)
+            gi = gaussian(mean_i, sigma_range)
+            gs = gaussian(mean_s, sigma_spatial)
             i_filtered += g[int(window_x)][int(window_y)] * gi * gs
             Wp += gi * gs
 
@@ -46,10 +46,10 @@ def jbf(f, g, filter_diameter, sigma_i, sigma_s):
     #g is filtered
     
     filtered_image = np.zeros(g.shape)
-    filtered_image =  Parallel(n_jobs=20)(delayed(apply_bilateral_filter)(f, g, i, j, filter_diameter, sigma_i, sigma_s) for i in range (len(g)) for j in range (len(g[0])))
+    filtered_image =  Parallel(n_jobs=30)(delayed(apply_bilateral_filter)(f, g, i, j, filter_diameter, sigma_i, sigma_s) for i in range (len(g)) for j in range (len(g[0])))
     return np.array(filtered_image, dtype = np.uint8).reshape(g.shape)
 
-def upsampling(rgb, depth):
+def upsampling(rgb, depth, s1, s2):
     
     rgb_ori = rgb.copy()
     #upsampling fector based on rows
@@ -67,15 +67,17 @@ def upsampling(rgb, depth):
         rgb = cv2.resize(rgb, dim) 
         depth = cv2.resize(depth, dim) 
 
-        depth = jbf(rgb, depth, 5, 10, 10)
+        depth = jbf(rgb, depth, 5, s1, s2)
 
     
     depth = cv2.resize(depth, (rgb_ori.shape[1], rgb_ori.shape[0]))
-    return jbf(rgb_ori, depth, 5, 10, 10)
+    return jbf(rgb_ori, depth, 5, s1, s2)
 
 
 if __name__ == "__main__":
 
+    SIGMA_SPATIAL = 5
+    SIGNA_RANGE = 5
 
     rgb = cv2.imread("view5.png", 0)
     depth = cv2.imread("lowres_depth.png", 0)
@@ -86,8 +88,10 @@ if __name__ == "__main__":
     # dim = (width, height)
     # rgb = cv2.resize(rgb, dim)
     # print(rgb.shape)
-
-    upsampled = upsampling(rgb, depth)
+    
+    start=datetime.now()
+    upsampled = upsampling(rgb, depth, SIGMA_SPATIAL, SIGNA_RANGE)
+    print(datetime.now()-start)
 
     cv2.imwrite("upsampeld.png",upsampled)
 
