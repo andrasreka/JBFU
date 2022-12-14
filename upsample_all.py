70b23f9d5684c2629689c096916486bdc83eaa12
andrasreka <reka@mollia.hu>
2022-12-15 00:37:20 +0200

    fix output paths

--- upsample_all.py
+++ upsample_all.py
@@ -16,16 +16,16 @@ if __name__ == "__main__":
         d = cv2.imread(base + image + "/disp1.png", 0)
 
         jb_sf = upsampling(rgb, d, kernel, sigma_spatial, sigma_range)
-        cv2.imwrite(base + image + "jb_sf.png",jb_sf)
+        cv2.imwrite(base + image + "/jb_sf.png",jb_sf)
 
         jb_it = upsampling_iterative(rgb, d, kernel, sigma_spatial, sigma_range)
-        cv2.imwrite(base + image + "jb_it.png",jb_it)
+        cv2.imwrite(base + image + "/jb_it.png",jb_it)
 
         jbm_sf = upsampling(rgb, d, kernel, sigma_spatial, sigma_range, weights)
-        cv2.imwrite(base + image + "jbm_sf.png",jbm_sf)
+        cv2.imwrite(base + image + "/jbm_sf.png",jbm_sf)
 
         jbm_it = upsampling_iterative(rgb, d, kernel, sigma_spatial, sigma_range, weights)
-        cv2.imwrite(base + image + "jbm_it.png",jbm_it)
+        cv2.imwrite(base + image + "/jbm_it.png",jbm_it)
 
 
 
